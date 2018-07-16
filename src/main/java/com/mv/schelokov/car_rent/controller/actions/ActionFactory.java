71e345d8f828c4146e05eceb68325e4357321d68
Maxim Chshelokov <schelokov.mv@gmail.com>
2018-07-16 08:39:49 +0600

    Changed switch to HashMap

--- src/main/java/com/mv/schelokov/car_rent/controller/actions/ActionFactory.java
+++ src/main/java/com/mv/schelokov/car_rent/controller/actions/ActionFactory.java
@@ -1,5 +1,6 @@
 package com.mv.schelokov.car_rent.controller.actions;
 
+import com.mv.schelokov.car_rent.controller.actions.admin.DeleteCar;
 import com.mv.schelokov.car_rent.controller.consts.Urls;
 import com.mv.schelokov.car_rent.controller.actions.admin.DeleteUser;
 import com.mv.schelokov.car_rent.controller.actions.admin.ShowCarList;
@@ -9,6 +10,8 @@ import com.mv.schelokov.car_rent.controller.actions.admin.ShowUsersList;
 import com.mv.schelokov.car_rent.controller.actions.admin.UpdateCar;
 import com.mv.schelokov.car_rent.controller.actions.admin.UpdateUser;
 import com.mv.schelokov.car_rent.controller.consts.Jsps;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 
@@ -17,38 +20,30 @@ import org.apache.log4j.Logger;
  * @author Maxim Chshelokov <schelokov.mv@gmail.com>
  */
 public class ActionFactory {
-    private static final Logger log = Logger.getLogger(ActionFactory.class);
+    private static final Logger LOG = Logger.getLogger(ActionFactory.class);
+    private static final Map<String, Action> ACTIONS = new HashMap<>();
+    static {
+        ACTIONS.put("home", new ShowPage(Jsps.HOME));
+        ACTIONS.put("login", new Login());
+        ACTIONS.put("admin_actions", new ShowPage(Jsps.ADMIN_ACTIONS));
+        ACTIONS.put("user_list", new ShowUsersList());
+        ACTIONS.put("edit_user", new ShowEditUserPage());
+        ACTIONS.put("update_user", new UpdateUser());
+        ACTIONS.put("delete_user", new DeleteUser());
+        ACTIONS.put("car_list", new ShowCarList());
+        ACTIONS.put("edit_car", new ShowEditCarPage());
+        ACTIONS.put("update_car", new UpdateCar());
+        ACTIONS.put("delete_car", new DeleteCar());
+    }
     
     public static Action action(HttpServletRequest req) {
-        String actionName = req.getPathInfo().replaceAll("/", "").toUpperCase();
-        log.debug(String.format("Forward to %s", actionName));
+        String actionName = req.getPathInfo().replaceAll("/", "").toLowerCase();
+        LOG.debug(String.format("Forward to %s", actionName));
         if ("".equals(actionName))
-            return new ShowPage(Jsps.HOME);
-        switch(Urls.valueOf(actionName)) {
-            case HOME:
-                return new ShowPage(Jsps.HOME);
-            case LOGIN:
-                return new Login();
-//            case WELCOME:
-//                return new ShowPage("view/welcome.jsp");
-            case ADMIN_ACTIONS:
-                return new ShowPage(Jsps.ADMIN_ACTIONS);
-            case USER_LIST:
-                return new ShowUsersList();
-            case EDIT_USER:
-                return new ShowEditUserPage();
-            case UPDATE_USER:
-                return new UpdateUser();
-            case DELETE_USER:
-                return new DeleteUser();
-            case CAR_LIST:
-                return new ShowCarList();
-            case EDIT_CAR:
-                return new ShowEditCarPage();
-            case UPDATE_CAR:
-                return new UpdateCar();
-            default:
-                return new ShowPage(Jsps.HOME);                
-        }
+            return ACTIONS.get("home");
+        Action result = ACTIONS.get(actionName);
+        if (result == null)
+            return ACTIONS.get("home");     // Change to 404 page
+        return result;
     }
 }
